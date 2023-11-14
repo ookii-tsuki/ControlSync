6addfb9ac13b914b00e83a924ec07a8ec2a7fbab
Mohamed Amin Ben Chrifa <mohamed6aminbenchrifa@gmail.com>
2023-11-14 20:54:59 +0100

    flexible stream resolution

--- ControlSync/Client/Screenshare.cs
+++ ControlSync/Client/Screenshare.cs
@@ -1,27 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SharpDX;
-using SharpDX.DXGI;
+﻿using SharpDX;
+using SharpDX.Direct2D1;
 using SharpDX.Direct3D11;
-using Device = SharpDX.Direct3D11.Device;
-using MapFlags = SharpDX.Direct3D11.MapFlags;
-using SharpDX.Direct3D;
-using System.Windows.Media;
+using SharpDX.DXGI;
+using SharpDX.Mathematics.Interop;
+using SIPSorceryMedia.Abstractions;
+using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
-using LZ4;
-using System.Diagnostics;
-using System.IO;
-using System.Windows.Media.Imaging;
-using System.Windows;
-using SharpDX.WIC;
-using SharpDX.Direct2D1;
-using SharpDX.IO;
+using D2D = SharpDX.Direct2D1;
+using Device = SharpDX.Direct3D11.Device;
 using Factory1 = SharpDX.DXGI.Factory1;
-using SIPSorceryMedia.Abstractions;
 
 namespace ControlSync.Client
 {
@@ -39,13 +28,11 @@ namespace ControlSync.Client
         static Device device;
         static Output output;
         static Output1 output1;
-        static int screenWidth, streamWidth;
-        static int screenHeight, streamHeight;
-        static Texture2D screenTexture, smallerTexture;
-        static ShaderResourceView smallerTextureView;
+        static int streamWidth;
+        static int streamHeight;
         static OutputDuplication duplicatedOutput;
+        static OutputDescription description;
 
-        public static ScreenView view; // just for now
         private static void Init()
         {
             initilized = true;
@@ -55,54 +42,16 @@ namespace ControlSync.Client
             adapter = factory.GetAdapter1(numAdapter);
 
             // Create device from Adapter
-            device = new Device(adapter);
+            device = new Device(adapter, DeviceCreationFlags.BgraSupport);
 
             // Get DXGI.Output
             output = adapter.GetOutput(numOutput);
             output1 = output.QueryInterface<Output1>();
 
-            var description = output1.Description;
+            description = output1.Description;
 
-            // Width/Height of desktop to capture
-            screenWidth = description.DesktopBounds.Right - description.DesktopBounds.Left;
-            screenHeight = description.DesktopBounds.Bottom - description.DesktopBounds.Top;
-
-            streamWidth = screenWidth / 2;
-            streamHeight = screenHeight / 2;
-
-            // Create Staging texture CPU-accessible
-            var textureDesc = new Texture2DDescription
-            {
-                CpuAccessFlags = CpuAccessFlags.Read,
-                BindFlags = BindFlags.None,
-                Format = Format.B8G8R8A8_UNorm,
-                Width = streamWidth,
-                Height = streamHeight,
-                OptionFlags = ResourceOptionFlags.None,
-                MipLevels = 1,
-                ArraySize = 1,
-                SampleDescription = { Count = 1, Quality = 0 },
-                Usage = ResourceUsage.Staging
-            };
-
-            screenTexture = new Texture2D(device, textureDesc);
-
-            // Create Staging texture CPU-accessible
-            var smallerTextureDesc = new Texture2DDescription
-            {
-                CpuAccessFlags = CpuAccessFlags.None,
-                BindFlags = BindFlags.RenderTarget | BindFlags.ShaderResource,
-                Format = Format.B8G8R8A8_UNorm,
-                Width = screenWidth,
-                Height = screenHeight,
-                OptionFlags = ResourceOptionFlags.GenerateMipMaps,
-                MipLevels = 4,
-                ArraySize = 1,
-                SampleDescription = { Count = 1, Quality = 0 },
-                Usage = ResourceUsage.Default
-            };
-            smallerTexture = new Texture2D(device, smallerTextureDesc);
-            smallerTextureView = new ShaderResourceView(device, smallerTexture);
+            streamWidth = 1280;
+            streamHeight = 720;
 
             // Duplicate the output
             duplicatedOutput = output1.DuplicateOutput(device);
@@ -147,6 +96,7 @@ namespace ControlSync.Client
 
             try
             {
+                var s = Stopwatch.StartNew();
                 SharpDX.DXGI.Resource screenResource;
                 OutputDuplicateFrameInformation duplicateFrameInformation;
 
@@ -156,20 +106,79 @@ namespace ControlSync.Client
                 if (screenResource == null)
                     return null;
 
-                // copy resource into memory that can be accessed by the CPU
-                using (var screenTexture2D = screenResource.QueryInterface<Texture2D>())
-                    device.ImmediateContext.CopySubresourceRegion(screenTexture2D, 0, null, smallerTexture, 0);
+                using var dxgiDevice = device.QueryInterface<SharpDX.DXGI.Device>();
+                using var d2dFactory = new D2D.Factory1();
+                using var d2dDevice = new D2D.Device(d2dFactory, dxgiDevice);
+                using var frameDc = new D2D.DeviceContext(d2dDevice, D2D.DeviceContextOptions.EnableMultithreadedOptimizations);
+                using var frameSurface = screenResource.QueryInterface<Surface>();
+                using var frameBitmap = new D2D.Bitmap1(frameDc, frameSurface);
 
-                device.ImmediateContext.GenerateMips(smallerTextureView);
+                // create a GPU resized texture/surface/bitmap
+                var desc = new Texture2DDescription
+                {
+                    CpuAccessFlags = CpuAccessFlags.Read, // only GPU
+                    BindFlags = BindFlags.RenderTarget, // to use D2D
+                    Format = Format.B8G8R8A8_UNorm,
+                    Width = streamWidth,
+                    Height = streamHeight,
+                    OptionFlags = ResourceOptionFlags.None,
+                    MipLevels = 1,
+                    ArraySize = 1,
+                    SampleDescription = { Count = 1, Quality = 0 },
+                    Usage = ResourceUsage.Default
+                };
+                using var texture = new Texture2D(device, desc);
 
-                device.ImmediateContext.CopySubresourceRegion(smallerTexture, 1, null, screenTexture, 0);
+                using var textureDc = new D2D.DeviceContext(d2dDevice, D2D.DeviceContextOptions.EnableMultithreadedOptimizations); // create a D2D device context
+                using var textureSurface = texture.QueryInterface<Surface>(); // this texture is a DXGI surface
+                using var textureBitmap = new D2D.Bitmap1(textureDc, textureSurface); // we can create a GPU bitmap on a DXGI surface
 
+                // associate the DC with the GPU texture/surface/bitmap
+                textureDc.Target = textureBitmap;
+
+                // this is were we draw on the GPU texture/surface
+                textureDc.BeginDraw();
+
+                // this will automatically resize
+                textureDc.DrawBitmap(
+                    frameBitmap,
+                    new RawRectangleF(0, 0, streamWidth, streamHeight),
+                    1,
+                    D2D.InterpolationMode.HighQualityCubic, // change this for quality vs speed
+                    null,
+                    null);
+
+                // commit draw
+                textureDc.EndDraw();
+
+
+                using var texture2dc = new D2D.DeviceContext(d2dDevice, D2D.DeviceContextOptions.EnableMultithreadedOptimizations); // create a D2D device context
+
+                var desc2 = new Texture2DDescription
+                {
+                    CpuAccessFlags = CpuAccessFlags.Read, // only GPU
+                    BindFlags = BindFlags.None, // to use D2D
+                    Format = Format.B8G8R8A8_UNorm,
+                    Width = streamWidth,
+                    Height = streamHeight,
+                    OptionFlags = ResourceOptionFlags.None,
+                    MipLevels = 1,
+                    ArraySize = 1,
+                    SampleDescription = { Count = 1, Quality = 0 },
+                    Usage = ResourceUsage.Staging
+                };
+                using var texture2 = new Texture2D(device, desc2);
+
+                using var texture2Surface = texture2.QueryInterface<Surface>();
+                using Bitmap1 bitmap1 = new Bitmap1(texture2dc, texture2Surface);
+
+                bitmap1.CopyFromBitmap(textureBitmap);
                 // Get the desktop capture texture
-                var mapSource = device.ImmediateContext.MapSubresource(screenTexture, 0, MapMode.Read, MapFlags.None);
+                var mapSource = bitmap1.Map(MapOptions.Read);
 
                 // Create Drawing.Bitmap
                 var bitmap = new System.Drawing.Bitmap(streamWidth, streamHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-                var boundsRect = new System.Drawing.Rectangle(0, 0, streamWidth, streamHeight);
+                var boundsRect = new Rectangle(0, 0, streamWidth, streamHeight);
 
                 // Copy pixels from screen capture Texture to GDI bitmap
                 var mapDest = bitmap.LockBits(boundsRect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
@@ -181,20 +190,17 @@ namespace ControlSync.Client
                     Utilities.CopyMemory(destPtr, sourcePtr, (streamWidth) * 4);
 
                     // Advance pointers
-                    sourcePtr = IntPtr.Add(sourcePtr, mapSource.RowPitch);
+                    sourcePtr = IntPtr.Add(sourcePtr, mapSource.Pitch);
                     destPtr = IntPtr.Add(destPtr, mapDest.Stride);
                 }
 
                 // Release source and dest locks
                 bitmap.UnlockBits(mapDest);
-                device.ImmediateContext.UnmapSubresource(screenTexture, 0);
-
-                // Save the output
-                //bitmap.Save(outputFileName);
+                bitmap1.Unmap();
                 screenResource.Dispose();
                 duplicatedOutput.ReleaseFrame();
 
-
+                s.Stop();
 
                 return bitmap;
 
